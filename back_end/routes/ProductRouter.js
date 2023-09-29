14daddf0f4259528d8ff67534f7000e4960757b3
Krushi Shah <krushishah@MacBook-Pro-149.local>
2023-09-29 17:16:59 -0400

    added controllers and product routes

--- back_end/routes/ProductRouter.js
+++ back_end/routes/ProductRouter.js
@@ -4,7 +4,17 @@ const router = express.Router()
 
 // routes
 router.get("/", (req, res) => {
-    res.json({mssg: "Welcome to app"})
+    res.json({ mssg: "get all products" })
+})
+
+router.get("/:id", (req, res) => {
+    req.params;
+    res.json(req.params)
+})
+
+router.POST("/:name", (req, res) => {
+    req.params;
+    res.json(req.params)
 })
 
 module.exports = router
