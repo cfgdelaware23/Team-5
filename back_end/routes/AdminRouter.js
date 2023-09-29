11f09174de4a4585f2f3efd6e87fa0c6871fece8
yasasvit <yt397@scarletmail.rutgers.edu>
2023-09-29 18:21:08 -0400

    get request

--- back_end/routes/AdminRouter.js
+++ back_end/routes/AdminRouter.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const feedback = require("./models/feedback.js")
 const products = require("./models/product.js")
+const users = require("./models/customers.js")
 const router = express.Router()
 
 // routes
@@ -12,9 +13,17 @@ router.get("/feedback", async (req, res) => {
     res.status(200).json(feedback)
 
 })
+
 // Gets all products 
 router.get("/products_all", async (req, res) => {
     const products = await products.find({}).sort({quantitySold: -1})
+    
+    res.status(200).json(products)
+})
+
+// Gets all customers
+router.get("/customers_all", async (req, res) => {
+    const customers = await user.find({}).sort({createdAt: -1})
     
     res.status(200).json(products)
 })
