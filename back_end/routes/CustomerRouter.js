ffdba33501b0f36031ef4da26ed3e93535d4b32c
ViktorBash <basharkevichv@gmail.com>
2023-09-29 18:07:13 -0400

    Finished /save_customer route

--- back_end/routes/CustomerRouter.js
+++ back_end/routes/CustomerRouter.js
@@ -1,9 +1,20 @@
 const express = require("express")
+const Customer = require("../models/user")
 
 const router = express.Router()
 
 router.post("/save_customer", (request, res) => {
-    console.log(request.body);
+    let name = request.body.name;
+    let qualify = request.body.qualify;
+    let address = request.body.address;
+    let isAdmin = request.body.isAdmin;
+
+    const customer = Customer.create({name, qualify, address, isAdmin})
+    
+    console.log(customer);
+
+    res.json({ mssg: "customer created successfully" })
+    return;
 })
 
 router.post("/retrieve_customer", (request, res) => {
