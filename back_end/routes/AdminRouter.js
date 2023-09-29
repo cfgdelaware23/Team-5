1d5e7380483aa895877f8039e198bc20b1688a19
yasasvit <yt397@scarletmail.rutgers.edu>
2023-09-29 18:15:35 -0400

    admin router requests

--- back_end/routes/AdminRouter.js
+++ back_end/routes/AdminRouter.js
@@ -1,16 +1,22 @@
 const express = require("express")
-
+const feedback = require("./models/feedback.js")
+const products = require("./models/product.js")
 const router = express.Router()
 
 // routes
 
 // Gets all feedback in JSON array
-router.get("/feedback", (req, res) => {
-    res.json({mssg: "Feedback"})
+router.get("/feedback", async (req, res) => {
+    const feedback = await feedback.find({}).sort({createdAt: -1})
+
+    res.status(200).json(workouts)
+
 })
 // Gets all products 
-router.get("/products_all", (req, res) => {
-    res.json({mssg: "All products"})
+router.get("/products_all", async (req, res) => {
+    const products = await products.find({}).sort({quantitySold: -1})
+    
+    res.status(200).json(workouts)
 })
 
 module.exports = router
