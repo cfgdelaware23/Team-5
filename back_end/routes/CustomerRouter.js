d3a269efeabd3fa9adf5be1d4f1691b3d9809131
ViktorBash <basharkevichv@gmail.com>
2023-09-29 18:25:58 -0400

    Finished /retrieve_customer/:id and change other routes to async in CustomerRouter.js

--- back_end/routes/CustomerRouter.js
+++ back_end/routes/CustomerRouter.js
@@ -3,13 +3,13 @@ const Customer = require("../models/user")
 
 const router = express.Router()
 
-router.post("/save_customer", (request, res) => {
+router.post("/save_customer", async (request, res) => {
     let name = request.body.name;
     let qualify = request.body.qualify;
     let address = request.body.address;
     let isAdmin = request.body.isAdmin;
 
-    const customer = Customer.create({name, qualify, address, isAdmin})
+    const customer = await Customer.create({name, qualify, address, isAdmin})
     
     console.log(customer);
 
@@ -17,11 +17,21 @@ router.post("/save_customer", (request, res) => {
     return;
 })
 
-router.post("/retrieve_customer", (request, res) => {
-    console.log(request.body);
+router.get("/retrieve_customer/:id", async (request, res) => {
+    let passedInId = request.params.id;
+    try {
+        let customer = await Customer.findById(passedInId);
+        console.log(customer);
+        res.status(200).json(customer);
+    }
+    catch(err) {
+        res.json({mssg: "customer not found"})
+        return;
+    }
+    return;
 })
 
-router.post("/feedback_create", (request, res) => {
+router.post("/feedback_create", async (request, res) => {
     console.log(request.body);
 })
 
