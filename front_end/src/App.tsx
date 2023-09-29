08a2ca9e22dda9197e0124695463f2f75c19b14a
Emily Sun <ejs83@duke.edu>
2023-09-29 19:40:25 -0400

    transaction tables

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import SignupPage from './pages/signupPage'
+import SignupPage from './pages/SignupPage'
 import './App.css'
 // import SideBar from './components/SideBar'
 
