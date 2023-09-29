6e6fe19625de0305b12daadea95d418221374d5b
ViktorBash <basharkevichv@gmail.com>
2023-09-29 17:18:39 -0400

    Added shell for customer routes

--- back_end/routes/CustomerRouter.js
+++ back_end/routes/CustomerRouter.js
@@ -2,8 +2,16 @@ const express = require("express")
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.json({mssg: "Welcome to app"})
+router.post("/save_customer", (request, res) => {
+    console.log(request.body);
+})
+
+router.post("/retrieve_customer", (request, res) => {
+    console.log(request.body);
+})
+
+router.post("/feedback_create", (request, res) => {
+    console.log(request.body);
 })
 
 module.exports = router
