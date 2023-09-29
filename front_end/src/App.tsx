3889ab44cd3106b521a2aa1716b3d12bbb4eef5e
Emily Sun <ejs83@duke.edu>
2023-09-29 19:59:51 -0400

    initial table commit

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import SignupPage from './pages/SignupPage'
 import './App.css'
+import TransactionTable from './components/TransactionTable'
 // import SideBar from './components/SideBar'
 
 function App() {
 
   return (
     <>  
-<SignupPage></SignupPage> 
-
+<TransactionTable></TransactionTable>
     </>
   )
 }
