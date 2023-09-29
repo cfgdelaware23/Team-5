6caa564c1a051dd95676a4e2875f08a409f07239
Emily Sun <ejs83@duke.edu>
2023-09-29 17:15:53 -0400

    start signup page

---
+++ front_end/src/pages/signupPage.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function signupPage() {
+  const [count, setCount] = useState(0)
+  const [firstName, setFirstName] = useState(0)
+  const [lastName, setLastName] = useState(0)
+  const [zipCode, setZipCode] = useState(0)
+
+
+  return (
+    <>
+    <p>
+        hello world
+    </p>
+    </>
+  )
+}
+
+export default signupPage
