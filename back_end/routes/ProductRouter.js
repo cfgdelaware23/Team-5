cf6ad074d7157c314d26efc27bd7b98d6f7a3d3b
Krushi Shah <krushishah@MacBook-Pro-149.local>
2023-09-29 19:43:46 -0400

    updating routes and products

--- back_end/routes/ProductRouter.js
+++ back_end/routes/ProductRouter.js
@@ -1,20 +1,39 @@
 const express = require("express")
-
+const Customer = require("../models/product")
 const router = express.Router()
 
 // routes
-router.get("/", (req, res) => {
-    res.json({ mssg: "get all products" })
+router.get("/", async(req, res) => {
+    const products = await products.find({}).sort({ quantitySold: -1 })
+
+    res.status(200).json(products)
 })
 
-router.get("/:id", (req, res) => {
-    req.params;
-    res.json(req.params)
+router.get("/get_product/:id", async(request, res) => {
+    let passedInId = request.params.id;
+    try {
+        let product = await Product.findById(passedInId);
+        console.log(product);
+        res.status(200).json(product);
+    } catch (err) {
+        res.json({ mssg: "product not found" })
+        return;
+    }
+    return;
 })
 
-router.POST("/:name", (req, res) => {
-    req.params;
-    res.json(req.params)
+router.POST("/update_product/:id", async(request, res) => {
+    let id = request.body.id;
+
+    const product = await Product.findById(productId);
+    if (!product) {
+        return res.status(404).json({ message: 'Product not found' });
+    }
+
+    product.quantitySold += 1;
+
+    res.json({ mssg: "product incremented" })
+    return;
 })
 
 module.exports = router
