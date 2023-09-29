ea2ab691a41163f25dba3890536cf2fb9e82e4d6
yasasvit <yt397@scarletmail.rutgers.edu>
2023-09-29 16:29:33 -0400

    express setup

--- back_end/server.js
+++ back_end/server.js
@@ -1,1 +1,9 @@
 const express = require("express")
+
+// set up express app
+const app = express()
+
+// listening for requests
+app.listen(4000, () => {
+    console.log("Listening in port 4000")
+})
