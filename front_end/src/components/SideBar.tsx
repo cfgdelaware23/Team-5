61fa04a1e59d7e2463ca67771bf356d4cdd7f8d9
Nicholas Rios <rios.nicholas@protonmail.com>
2023-09-29 17:26:57 -0400

    Create SideBar component

---
+++ front_end/src/components/SideBar.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from "react";
+import "../App.css";
+
+const SideBar = () => {
+    return (
+        <aside>
+            <div id="wellfare-logo">
+                
+            </div>
+            <nav className="flex flex-col bg-blue-400">
+                test
+                <a>Home</a>
+                <a>Sales</a>
+                <a>Feedback</a>
+                <a>Customers</a>
+            </nav>
+        </aside>
+    );
+}
+
+export default SideBar;
