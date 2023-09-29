d578ca079a48f9dd5e521def5895f8bf7bdd3bd1
Nicholas Rios <rios.nicholas@protonmail.com>
2023-09-29 18:36:10 -0400

    Create DashboardGrid component

---
+++ front_end/src/components/DashboardGrid.tsx
@@ -0,0 +1,15 @@
+import "../App.css";
+// import DashboardCard from "./DashboardCard";
+
+const DashboardGrid = () => {
+    return (
+        <div className="grid">
+            {/* <DashboardCard /> 
+            <DashboardCard /> 
+            <DashboardCard title="" /> 
+            <DashboardCard />  */}
+        </div>
+    );
+}
+
+export default DashboardGrid;
