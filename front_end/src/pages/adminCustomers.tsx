ee72d408842b8a0939c66d27efb53f09a1825997
Emily Sun <ejs83@duke.edu>
2023-09-30 02:47:07 -0400

    admin customer style

--- front_end/src/pages/adminCustomers.tsx
+++ front_end/src/pages/adminCustomers.tsx
@@ -5,17 +5,44 @@ import CustomerCount from '../components/CustomerCount'
 import SideBar from '../components/AdminSideBar'
 
 
-function adminCustomers() {
+
+const adminCustomers = () => {
+    const [customers, setCustomers] = useState(null)
+
+    useEffect(() => {
+
+        const fetchCustomers = async () => {
+            const response = await fetch("http://localhost:4000/admin/customers_all");
+            const json = await response.json()
+
+
+            if (response.ok) {
+                setCustomers(json)
+            }
+        }
+        fetchCustomers()
+    }, [])
+
 
     return (
         <div className='flex h-full w-full'>
             <SideBar />
             <div className='flex flex-col'>
-                <h1>Customer Summary</h1>
-                
-                <NewCustomerTable />
+                    <div className="left-0 px-4 py-4">
+                        {customers != null &&
+                            <div className="bg-white p-6 rounded-lg shadow-lg">
+                                <h2 className="text-xl font-bold mb-2 text-center">Total Customers: {customers.length}</h2>
+                            </div>
+                        }
+                    </div>
+                    <div>
+                    <NewCustomerTable />
+
+                </div>
+                </div>
+
+
             </div>
-        </div>
     )
 }
 
