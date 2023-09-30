7c46f500581e5e6fd458a7b70f17f28b6d716fd0
Nicholas Rios <rios.nicholas@protonmail.com>
2023-09-30 10:17:06 -0400

    Fix table overflow issue

--- front_end/src/pages/adminSales.tsx
+++ front_end/src/pages/adminSales.tsx
@@ -34,41 +34,42 @@ const AdminSales = () => {
 
 
     return (
-        <div className="flex h-full w-full">
+        <div className="flex h-screen w-screen">
             <AdminSideBar />
             <div className="w-4/5vw mb-8 sm:mx-4 lg:mx-8">
-                <h1 className="text-3xl font-bold mb-2 text-left">Products Summary</h1>
-                <table className="min-w-full bg-white border border-gray-300">
-                    <thead>
-                        <tr>
-                            <th className="py-2 px-6 bg-red-700 text-white">Name</th>
-                            <th className="py-2 px-6 bg-red-700 text-white">Quantity Sold</th>
-                            <th className="py-2 px-6 bg-red-700 text-white">Price</th>
-                            <th className="py-2 px-6 bg-red-700 text-white">Discounted Price</th>
-                            <th className="py-2 px-6 bg-red-700 text-white">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {products &&
-                            products.map((p) => (
-                                <tr key={p._id} className="hover:bg-gray-100">
-                                    <td className="py-2 px-6 border-b">{p.name}</td>
-                                    <td className="py-2 px-6 border-b text-center">{p.quantitySold}</td>
-                                    <td className="py-2 px-6 border-b text-center">{p.priceFull}</td>
-                                    <td className="py-2 px-6 border-b text-center">{p.priceDiscount}</td>
-                                    <td className="py-2 px-6 border-b text-center">
-                                        <button
-                                            onClick={() => handleEditProduct(p._id)}
-                                            className="text-red-700 underline"
-                                            style={{ background: 'transparent', border: 'none', cursor: 'pointer' }}>
-                                            Edit
-                                        </button>
-                                    </td>
-                                </tr>
-
-                            ))}
-                    </tbody>
-                </table>
+                <div className="h-screen overflow-scroll relative p-4">
+                    <h1 className="text-3xl font-bold mb-2 text-left pt-4">Products Summary</h1>
+                    <table className="min-w-full bg-white border border-gray-300 absolute" style={{height: '50%'}}>
+                        <thead>
+                            <tr>
+                                <th className="py-2 px-6 bg-red-700 text-white">Name</th>
+                                <th className="py-2 px-6 bg-red-700 text-white">Quantity Sold</th>
+                                <th className="py-2 px-6 bg-red-700 text-white">Price</th>
+                                <th className="py-2 px-6 bg-red-700 text-white">Discounted Price</th>
+                                <th className="py-2 px-6 bg-red-700 text-white">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {products &&
+                                products.map((p) => (
+                                    <tr key={p._id} className="hover:bg-gray-100">
+                                        <td className="py-2 px-6 border-b">{p.name}</td>
+                                        <td className="py-2 px-6 border-b text-center">{p.quantitySold}</td>
+                                        <td className="py-2 px-6 border-b text-center">{p.priceFull}</td>
+                                        <td className="py-2 px-6 border-b text-center">{p.priceDiscount}</td>
+                                        <td className="py-2 px-6 border-b text-center">
+                                            <button
+                                                onClick={() => handleEditProduct(p._id)}
+                                                className="text-red-700 underline"
+                                                style={{ background: 'transparent', border: 'none', cursor: 'pointer' }}>
+                                                Edit
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
         </div>
