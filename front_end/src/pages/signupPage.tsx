ddb470eea14da085364052811808e44af27cc048
Nicholas Rios <rios.nicholas@protonmail.com>
2023-09-30 03:56:23 -0400

    Update styling for improved accessibility
    - Update CSS for improved contrast/readability
    - Add semantic tags
    - Update margins and padding

--- front_end/src/pages/signupPage.tsx
+++ front_end/src/pages/signupPage.tsx
@@ -41,7 +41,7 @@ function SignupPage() {
 
     return (
         <div className='flex flex-col w-screen h-screen justify-center align-middle text-center'>
-            <h1 className='font-extrabold text-4xl m-4'>User Signup</h1>
+            <h1 className='font-extrabold text-4xl m-4 uppercase'>Sign Up</h1>
             <form className='flex flex-col align-middle items-center font-bold text-left'>
                 <label>First Name:</label>
                     <input className="border-2 border-black-500"
@@ -65,30 +65,29 @@ function SignupPage() {
                     onChange={e => setZipCode(e.target.value)}
                     placeholder='Zip code' />
             </form>
-            <div>
-                <p>Please select all that apply:</p>
-            </div>
-            <div>
-                <label className="px-3">
-                    EBT: {' '}
-                    <input
-                        name="inputEBT"
-                        type="checkbox"
-                        checked={EBT_SNAP}
-                        onChange={e => setEBT_SNAP(e.target.checked)} />
-                </label>
-
-                <label>
-                    WIC: {' '}
-                    <input
-                        name="inputEBT"
-                        type="checkbox"
-                        checked={WIC}
-                        onChange={e => setWIC(e.target.checked)} />
-                </label>
+            <div className='mt-4 mb-2 mx-2'>
+                <em className='italic font-semibold'>Please select all that apply:</em>
+                <div className='flex justify-center'>
+                    <div className='mx-2'>
+                        <label className="font-medium">EBT: {' '}</label>
+                            <input
+                                name="inputEBT"
+                                type="checkbox"
+                                checked={EBT_SNAP}
+                                onChange={e => setEBT_SNAP(e.target.checked)} />
+                    </div>
+                    <div className='mx-2'>
+                        <label className="font-medium">WIC: {' '}</label>
+                            <input
+                                name="inputEBT"
+                                type="checkbox"
+                                checked={WIC}
+                                onChange={e => setWIC(e.target.checked)} />
+                    </div>
+                </div>
             </div>
             <div className="justify-center">
-                <button className='bg-slate-200 text-black' type="button" onClick={() => {
+                <button className='bg-slate-200 text-black mx-1 font-medium' type="button" onClick={() => {
                     setFirstName('');
                     setLastName('');
                     setZipCode('');
@@ -97,14 +96,12 @@ function SignupPage() {
                 }}>
                     Clear
                 </button>
-                <button className='bg-blue-600 text-white' type="button" onClick={handleSubmit}>
+                <button className='bg-blue-700 text-white mx-1 font-medium' type="button" onClick={handleSubmit}>
                     Submit
                 </button>
             </div>
             <div className="py-3">
-            <button onClick={goToLoginPage}>
-            Already have an account?
-          </button>
+                <a className='hover:cursor-pointer font-medium text-blue-700' onClick={goToLoginPage}>Already have an account?</a>
             </div>
         </div>
     )
