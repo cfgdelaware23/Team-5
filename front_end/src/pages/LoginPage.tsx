c3b8d9d863f4206ed202c7dfc2b74274435154b4
Emily Sun <ejs83@duke.edu>
2023-09-29 21:49:00 -0400

    cleaned login page with no routing

--- front_end/src/pages/LoginPage.tsx
+++ front_end/src/pages/LoginPage.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import Link from 'react-router-dom'
 import '../app.css'
 
 
@@ -7,10 +6,15 @@ import '../app.css'
 
 function LoginPage({}) {
     const [memberId, setMemberId] = useState('')
-
     const onRegisterPress = () =>{
         setMemberId('')
     }
+
+    const onLoginPress = () =>{
+        if(memberId.length > 0){
+    }
+
+}
 
 
 
@@ -32,24 +36,20 @@ function LoginPage({}) {
                     onChange={e => setMemberId(e.target.value)} />
             </div>
             <div>
-            <Link 
-                className="btn btn-pink"
-                role="button"
-                to="/"
-                onClick={onRegisterPress()}
-                > 
-                Button1
-                </Link> 
-                <Link to="/signup">
-                    <button type="button">
-                        Register New Member
-                    </button>
-                    </Link>
-                <Link to="/transactions">
-                    <button type="button">
-                        Submit
-                    </button>
-                    </Link>
+                <button type="button" onClick={() => {
+                    console.log(memberId)
+                    onRegisterPress()
+                }}>
+                    Register New Member
+                </button>
+            </div>
+            <div>
+                <button type="button" onClick={() => {
+                    console.log(memberId)
+                    onLoginPress()
+                }}>
+                    Submit
+                </button>
             </div>
         </div>
     )
