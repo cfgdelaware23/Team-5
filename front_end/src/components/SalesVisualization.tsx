df521c3d353002f7e3a50f070581f9b33aa48127
Nicholas Rios <rios.nicholas@protonmail.com>
2023-09-30 09:55:45 -0400

    Create SalesVisualization component

---
+++ front_end/src/components/SalesVisualization.tsx
@@ -0,0 +1,14 @@
+import * as d3 from "d3";
+
+const SalesVisualization = () => {
+    // TODO: Initialize D3 and create bar plot
+    // let x = d3.
+    return (
+        <>
+            <div id='total-sales-viz'>
+            </div>
+        </>
+    );
+}
+
+export default SalesVisualization;
