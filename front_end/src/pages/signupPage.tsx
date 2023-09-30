b7a6f6ccc6501184d75185890d0f532517eb7b43
Nicholas Rios <rios.nicholas@protonmail.com>
2023-09-30 03:41:51 -0400

    Fix CSS centering issue
    - Remove inline CSS and replaced with tailwind classes
    - Add placeholder text

--- front_end/src/pages/signupPage.tsx
+++ front_end/src/pages/signupPage.tsx
@@ -40,39 +40,33 @@ function SignupPage() {
       };
 
     return (
+        <div className='flex flex-col w-screen h-screen justify-center align-middle text-center'>
+            <h1 className='font-extrabold text-4xl m-4'>User Signup</h1>
+            <form className='flex flex-col align-middle items-center font-bold text-left'>
+                <label>First Name:</label>
+                    <input className="border-2 border-black-500"
+                        name="inputFirstName"
+                        value={firstName}
+                        required={true}
+                        onChange={e => setFirstName(e.target.value)} placeholder='First name' />
 
-        <div style={{ textAlign: 'center', fontFamily: 'Helvetica' }}>
-            <h1 style={{ color: 'black', fontWeight: 'bold' }}>
-                User Signup
-            </h1>
-            <div>
-                <div>
-                    First Name:
-                </div>
-
-                <input className="border-2 border-black-500"
-                    name="inputFirstName"
-                    value={firstName}
-                    required={true}
-                    onChange={e => setFirstName(e.target.value)} />
-                <div >
-                    Last Name:
-                </div>
+                <label>Last Name:</label>
                 <input className="border-2 border-black-500"
                     name="inputSecondName"
                     value={lastName}
                     required={true}
-                    onChange={e => setLastName(e.target.value)} />
-                <div>
-                    Zip Code:
-                </div>
+                    onChange={e => setLastName(e.target.value)} 
+                    placeholder='Last name' />
+
+                <label>Zip Code:</label>
                 <input className="border-2 border-black-500"
                     name="inputZipCode"
                     value={zipCode}
-                    onChange={e => setZipCode(e.target.value)} />
-            </div>
+                    onChange={e => setZipCode(e.target.value)}
+                    placeholder='Zip code' />
+            </form>
             <div>
-                <p> Please select all that apply:</p>
+                <p>Please select all that apply:</p>
             </div>
             <div>
                 <label className="px-3">
@@ -94,24 +88,21 @@ function SignupPage() {
                 </label>
             </div>
             <div className="justify-center">
-                <button type="button" onClick={() => {
+                <button className='bg-slate-200 text-black' type="button" onClick={() => {
                     setFirstName('');
                     setLastName('');
                     setZipCode('');
                     setEBT_SNAP(false);
                     setWIC(false);
-                }} style={{ backgroundColor: 'rgb(214,60,35)', color: 'black', marginTop: '10px', marginRight: '20px' }}>
+                }}>
                     Clear
                 </button>
-                <button type="button" onClick={handleSubmit} style={{ backgroundColor: 'rgb(214,60,35)', color: 'black', marginTop: '10px', marginRight: '20px' }}>
+                <button className='bg-blue-600 text-white' type="button" onClick={handleSubmit}>
                     Submit
                 </button>
             </div>
             <div className="py-3">
-            <button
-            onClick={goToLoginPage}
-          style={{backgroundColor: 'rgb(214,60,35)', color: 'black', marginTop: '10px', marginLeft: "auto", marginRight: "auto", display: "block"}}
-          >
+            <button onClick={goToLoginPage}>
             Already have an account?
           </button>
             </div>
