b0f5bf76af911e40b9ab2c341b00063d75c1f44d
ViktorBash <basharkevichv@gmail.com>
2023-09-30 00:38:44 -0400

    Add shell HTML for submit feedback

--- front_end/src/pages/submitFeedback.tsx
+++ front_end/src/pages/submitFeedback.tsx
@@ -1,7 +1,40 @@
+import { useState } from 'react';
+
 function SubmitFeedback(){
+    const [memberId, setMemberId] = useState('');
+    const [isPositive, setIsPositive] = useState(false);
+    const [feedback, setFeedback] = useState('');
+
+    function handleSubmit () {
+
+    }
+
 return(
     <>
-    <p>Feedback form</p>
+    <h1>Submit Feedback</h1>
+        <input
+          name="inputMemberId"
+          value={memberId}
+          required={true}
+          onChange={(e) => setMemberId(e.target.value)}
+          style={{ backgroundColor: 'grey', color: 'white' }}
+        />
+        <input
+          name="feedback"
+          value={feedback}
+          required={true}
+          onChange={(e) => setFeedback(e.target.value)}
+          style={{ backgroundColor: 'grey', color: 'white' }}
+        />
+        <label>
+            Feedback positive?
+            <input
+                name="inputEBT"
+                type="checkbox"
+                checked={isPositive}
+                onChange={e => setIsPositive(e.target.checked)} />
+        </label>
+
     </>
 )
 }
