f70ebd70eef6b45061d626249dad16967e338f4a
Emily Sun <ejs83@duke.edu>
2023-09-30 01:34:49 -0400

    new table styling

--- front_end/src/components/tables/NewProductTable.tsx
+++ front_end/src/components/tables/NewProductTable.tsx
@@ -23,7 +23,7 @@ const NewProductTable = () => {
 
 
    return (
-       <div>
+       <div className="grid grid-cols-2 w-4/5vw h-full mb-8 sm:mx-4 lg:mx-8 gap-4">
            <table className="shadow-lg bg-white">
                    <tr>
                        <th className="bg-blue-100 border text-left px-8 py-4">Name</th>
