ad0125974fbc483fecdbe9c17d044709e8d09fad
Nicholas Rios <rios.nicholas@protonmail.com>
2023-09-29 23:51:28 -0400

    Update page route names

---
+++ front_end/src/pages/adminIndex.tsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+import './App.css'
+import DashboardGrid from '../components/DashboardGrid'
+import SideBar from '../components/SideBar'
+import Welcome from '../components/Welcome'
+
+function App() {
+
+  return (
+    <div className='flex h-full w-full'>
+      <SideBar />
+      <div className='flex flex-col'>
+        <Welcome />
+        <DashboardGrid />
+      </div>
+    </div>
+  )
+}
+
+export default App
