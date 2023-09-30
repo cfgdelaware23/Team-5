64141680bf0e2c53617b70fecf8b56cfa103a287
ViktorBash <basharkevichv@gmail.com>
2023-09-30 07:38:14 -0400

    Add more spacing for user sidebar

--- front_end/src/components/UserSideBar.tsx
+++ front_end/src/components/UserSideBar.tsx
@@ -14,7 +14,7 @@ const UserSideBar = () => {
     width: "100vw",
   }}>
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul className="font-medium flex flex-col p-4 pt-8 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-700 md:dark:bg-gray-900 dark:border-gray-700" style={{justifyContent: "center"}}>
+      <ul className="font-medium flex flex-col p-4 pt-8 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-700 md:dark:bg-gray-900 dark:border-gray-700" style={{justifyContent: "center", paddingTop: "10px", paddingBottom: "10px"}}>
         <li>
           <a href="/customer/products" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">See Products</a>
         </li>
