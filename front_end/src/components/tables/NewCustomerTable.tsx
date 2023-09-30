b4d4b178f7cd40c8a1536766cc8f6ff1f09767d2
yasasvit <yt397@scarletmail.rutgers.edu>
2023-09-30 01:32:28 -0400

    new customer table

---
+++ front_end/src/components/tables/NewCustomerTable.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+
+
+import "../App.css"
+
+
+const NewCustomerTable = () => {
+   const [customers, setCustomers] = useState(null)
+
+
+   useEffect(() => {
+       const fetchCustomers = async() => {
+           const response = await fetch("http://localhost:4000/admin/customers_all");
+           const json = await response.json()
+
+
+           if (response.ok) {
+               setCustomers(json)
+           }
+       }
+       fetchCustomers()
+   }, [])
+
+
+   return (
+       <div>
+           <table className="shadow-lg bg-white">
+                   <tr>
+                       <th className="bg-blue-100 border text-left px-8 py-4">Name</th>
+                       <th className="bg-blue-100 border text-left px-8 py-4">Qualify for Personalized Pricing</th>
+                       <th className="bg-blue-100 border text-left px-8 py-4">Address</th>
+                   </tr>
+                   {customers && customers.map(c => (
+                       <tr key={c._id}>
+                           <td className="border-black">{c.name}</td>
+                           <td className="border-black">{c.qualify}</td>
+                           <td className="border-black">{c.address}</td>
+                       </tr>
+                   ))}
+           </table>
+       </div>
+   )
+}
+
+
+export default NewCustomerTable
+
