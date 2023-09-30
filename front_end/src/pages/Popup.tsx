d0aa7eaad2d22939a091b4253d7ba5b302c11a73
yasasvit <yt397@scarletmail.rutgers.edu>
2023-09-30 08:29:45 -0400

    popup connect to backend

--- front_end/src/pages/Popup.tsx
+++ front_end/src/pages/Popup.tsx
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+import { useEffect, useState } from 'react'
 
 function Popup() {
   const [isOpen, setIsOpen] = useState(false);
+  const [product, setProduct] = useState(null)
+
+  useEffect(() => {
+    const fetchProduct = async() => {
+        const response = await fetch("http://localhost:4000/customer/get_random_product");
+        const json = await response.json()
+        if (response.ok) {
+            setProduct(json)
+        }
+    }
+    fetchProduct()
+}, [])
 
   const togglePopup = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <div className={`popup ${isOpen ? 'open' : ''}`}>
+    <div className={`popup ${isOpen ? 'open z-10 h-screen w-screen' : ''}`}>
       <div className="popup-content">
         <button onClick={togglePopup}>Close</button>
       </div>
