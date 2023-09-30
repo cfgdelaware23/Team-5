5b2beb838fd05510dc6379cb38086291ef088e7e
Emily Sun <ejs83@duke.edu>
2023-09-29 20:04:53 -0400

    formatting table

--- front_end/src/components/TransactionTable.tsx
+++ front_end/src/components/TransactionTable.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import "../App.css"
 
 function TransactionTable() {
@@ -6,42 +6,46 @@ function TransactionTable() {
 
     const fetchData = () => {
         fetch(`https://dummyjson.com/products`)
-        .then((response) => response.json())
-        .then((realdata) => {
-            console.log(realdata)
-            setData(realdata.products)
-            console.log(data)
-        })
-        .catch((err) => {
-            console.log(err.message)
-        })
+            .then((response) => response.json())
+            .then((realdata) => {
+                console.log(realdata)
+                setData(realdata.products)
+                console.log(data)
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
     }
     useEffect(() => {
         fetchData()
-    },[])
+    }, [])
 
     return (
         <div>
-            <tbody>
-                <tr>
-                    <th>Name</th>
-                    <th>Brand</th>
-                    <th>Image</th>
-                    <th>Price</th>
-                    <th>Rating</th>
-                </tr>
-                {data.map((item, index) => (
-          <tr key={index}>
-            <td>{item.title}</td>
-            <td>{item.brand}</td>
-            <td>
-              <img src={item.thumbnail} alt="" height={100} />
-            </td>
-            <td>{item.price}</td>
-            <td>{item.rating}</td>
-          </tr>
-        ))}
-            </tbody>
+            <table className="table-auto">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Brand</th>
+                        <th>Image</th>
+                        <th>Price</th>
+                        <th>Rating</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.title}</td>
+                            <td>{item.brand}</td>
+                            <td>
+                                <img src={item.thumbnail} alt="" height={100} />
+                            </td>
+                            <td>{item.price}</td>
+                            <td>{item.rating}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
