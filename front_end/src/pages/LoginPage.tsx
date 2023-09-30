b7daeb150bcaa9b9ef80a077420217b10c3b5bec
Emily Sun <ejs83@duke.edu>
2023-09-29 21:03:21 -0400

    starting login page

---
+++ front_end/src/pages/LoginPage.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import '../app.css'
+
+
+
+function SignupPage() {
+    const [memberId, setMemberId] = useState('')
+
+
+    return (
+
+        <div >
+            <h1>
+                Login
+            </h1>
+            <div>
+                    <div>
+                        Member ID:
+                    </div>
+
+                    <input
+                        name="inputMemberId"
+                        value={memberId}
+                        required = {true}
+                        onChange={e => setMemberId(e.target.value)} />
+            </div>
+        </div>
+    )
+}
+
+export default SignupPage
