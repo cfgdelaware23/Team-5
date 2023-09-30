cf9329ae2bc406e9db7dc66ecdeb78012af98fac
Nicholas Rios <rios.nicholas@protonmail.com>
2023-09-29 21:13:35 -0400

    Update responsive modifers

--- front_end/src/components/DashboardGrid.tsx
+++ front_end/src/components/DashboardGrid.tsx
@@ -3,7 +3,7 @@ import DashboardCard from "./DashboardCard";
 
 const DashboardGrid = () => {
     return (
-        <div className="grid col-span-1">
+        <div className="grid grid-cols-2 w-full sm:mx-4 lg:mx-8 gap-4">
             <DashboardCard header='Test' data='' /> 
             <DashboardCard header='Test' data='' /> 
             <DashboardCard header='Test' data='' /> 
