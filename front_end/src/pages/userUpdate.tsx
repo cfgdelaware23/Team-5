de420c84c0fae646c0f5c33cd7f28a78b7914aba
Emily Sun <ejs83@duke.edu>
2023-09-30 09:14:05 -0400

    formatting div tags

--- front_end/src/pages/userUpdate.tsx
+++ front_end/src/pages/userUpdate.tsx
@@ -61,69 +61,69 @@ const UserUpdate = () => {
 
     return (
         <>
-        <div className='flex h-full w-full'>
-            <AdminSideBar />
-            <div className='flex flex-col'>
+            <div className='flex h-full w-full'>
+                <AdminSideBar />
+                <div className='flex flex-col'>
                     <div className="left-0 px-4 py-4">
-        <div className='flex flex-col w-screen h-screen justify-center align-middle text-center'>
-            <h1 className='font-extrabold text-4xl m-4 uppercase'>Sign Up</h1>
-            <form className='flex flex-col align-middle items-center font-bold text-left'>
-                <label>Name</label>
-                <input id="name-id" className="border-2 border-black-500"
-                    name="inputSecondName"
-                    defaultValue={name}
-                    required={true}
-                    // onChange={e => setName(e.target.value)} 
-                    placeholder='Name' />
+                        <div className='flex flex-col w-screen h-screen justify-center align-middle text-center'>
+                            <h1 className='font-extrabold text-4xl m-4 uppercase'>Sign Up</h1>
+                            <form className='flex flex-col align-middle items-center font-bold text-left'>
+                                <label>Name</label>
+                                <input id="name-id" className="border-2 border-black-500"
+                                    name="inputSecondName"
+                                    defaultValue={name}
+                                    required={true}
+                                    // onChange={e => setName(e.target.value)} 
+                                    placeholder='Name' />
 
-                <label>Zip Code:</label>
-                <input id="zip-id" className="border-2 border-black-500"
-                    name="inputZipCode"
-                    defaultValue={zipCode}
-                    // onChange={e => setZipCode(e.target.value)}
-                    placeholder='Zip code' />
-            <div className='mt-4 mb-2 mx-2'>
-                <em className='italic font-semibold'>Please select all that apply:</em>
-                <div className='flex justify-center'>
-                    <div className='mx-2'>
-                        <label className="font-medium">EBT/WIC: {' '}</label>
-                            <input
-                            id="check-id"
-                                name="inputEBT"
-                                type="checkbox"
-                                defaultChecked={qualify}
-                                // onChange={e => setQualify(e.target.checked)} />
-                                />
-                    </div>
-                    <div className='mx-2'>
-                        <label className="font-medium">Is Admin:</label>
-                            <input
-                            id="admin-id"
-                                name="isAdmin"
-                                type="checkbox"
-                                defaultChecked={isAdmin}
-                                // onChange={e => setQualify(e.target.checked)} />
-                                />
-                    </div>
-                </div>
-            </div>
-            <div className="justify-center">
-                <button className='bg-blue-700 text-white mx-1 font-medium' type="button" onClick={handleSubmit}>
-                    Submit
-                </button>
-            </div>
-            </form>
-        </div>
+                                <label>Zip Code:</label>
+                                <input id="zip-id" className="border-2 border-black-500"
+                                    name="inputZipCode"
+                                    defaultValue={zipCode}
+                                    // onChange={e => setZipCode(e.target.value)}
+                                    placeholder='Zip code' />
+                                <div className='mt-4 mb-2 mx-2'>
+                                    <em className='italic font-semibold'>Please select all that apply:</em>
+                                    <div className='flex justify-center'>
+                                        <div className='mx-2'>
+                                            <label className="font-medium">EBT/WIC: {' '}</label>
+                                            <input
+                                                id="check-id"
+                                                name="inputEBT"
+                                                type="checkbox"
+                                                defaultChecked={qualify}
+                                            // onChange={e => setQualify(e.target.checked)} />
+                                            />
+                                        </div>
+                                        <div className='mx-2'>
+                                            <label className="font-medium">Is Admin:</label>
+                                            <input
+                                                id="admin-id"
+                                                name="isAdmin"
+                                                type="checkbox"
+                                                defaultChecked={isAdmin}
+                                            // onChange={e => setQualify(e.target.checked)} />
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="justify-center">
+                                    <button className='bg-blue-700 text-white mx-1 font-medium' type="button" onClick={handleSubmit}>
+                                        Submit
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
 
                     </div>
                     <div>
 
-                </div>
+                    </div>
                 </div>
 
 
             </div>
-        
+
         </>
     )
 }
